1eeac727d4169cb8b7fb69e2f76efc1d813f4b09
Bang-Raf <alfalimbany@gmail.com>
2024-05-27 20:01:38 +0700

    Add CSS to components

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const TodoItem = ({ todo }) => {
-    return <p>{todo.title}</p>
+    return <p style={ styles.todoItem }>{todo.title}</p>
+}
+
+const styles = {
+    todoItem: {
+        fontSize: '24px',
+        padding: '8px',
+        border: '1px solid #ccc',
+        borderRadius: '10px',
+    },
 }
 
 export default TodoItem
