9ea61a0669a10fba4beb46defbda4a0b1e85e5aa
alfalimbany <alfalimbany@gmail.com>
2024-06-04 14:20:59 +0000

    change Todos width to 80%

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,26 +1,27 @@
 import React from 'react'
 import TodoItem from './TodoItem'
 const Todos = ({todos, toggleCompleted, deleteToggleItem}) => {
-  return (
-    <div style={styles.container}>
-        {todos.map((todo) => {
-            return (
-                <TodoItem 
-                    key={todo.id} 
-                    todo={todo} 
-                    toggleCompleted={toggleCompleted} 
-                    deleteToggleItem={deleteToggleItem} 
-                />
-            )
-        })}
-    </div>
-  )
+    return (
+        <div style={styles.container}>
+            {todos.map((todo) => {
+                return (
+                    <TodoItem 
+                        key={todo.id} 
+                        todo={todo} 
+                        toggleCompleted={toggleCompleted} 
+                        deleteToggleItem={deleteToggleItem} 
+                    />
+                )
+            })}
+        </div>
+    )
 }
 
 const styles = {
     container: {
-        width: '100%',
+        width: '80%',
         margin: '0 auto',
+        
     },
 }
 
